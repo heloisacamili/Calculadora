41a000cb5893fe9180de871767509c69012fd833
heloisacamili <heloisa.camilii@gmail.com>
2022-09-08 14:59:41 -0300

    Calculadora em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
+  num : string = '';
+  operacao : string | undefined;
+  resolucao : string | undefined;
+  num2 : string = '';
+  valueNum : number | undefined;
+  valueNum2 : number | undefined;
+  resultado : string = '';
+  ValueResultado : number | undefined;
+
+  public deletar() {
+    location.reload();
+  }
+
+  public mostrarNum(algarismo : string) {
+    if (this.operacao == null) {
+      this.num += algarismo;
+      this.resolucao = this.num;
+    } else {
+      this.num2 += algarismo;
+      this.resolucao = this.num + this.operacao + this.num2;
+    }
+  }
+
+  public mostrarOperacao(op : string) {
+    this.operacao = op;
+    this.resolucao += this.operacao;
+  }
+
+  public mostrarResultado(igual : string) {
+    this.valueNum = parseFloat(this.num);
+    this.valueNum2 = parseFloat(this.num2);
+    if (this.operacao == "+") {
+      this.ValueResultado = this.valueNum + this.valueNum2;
+      this.resultado = '' + this.ValueResultado;
+      this.resolucao = this.resultado;
+      this.num = this.resultado;
+      this.num2 = '';
+    } else if (this.operacao == "-") {
+      this.ValueResultado = this.valueNum - this.valueNum2;
+      this.resultado = '' + this.ValueResultado;
+      this.resolucao = this.resultado;
+      this.num = this.resultado;
+      this.num2 = '';
+    } else if (this.operacao == "*") {
+      this.ValueResultado = this.valueNum * this.valueNum2;
+      this.resultado = '' + this.ValueResultado;
+      this.resolucao = this.resultado;
+      this.num = this.resultado;
+      this.num2 = '';
+    } else {
+      this.ValueResultado = this.valueNum / this.valueNum2;
+      this.resultado = '' + this.ValueResultado;
+      this.resolucao = this.resultado;
+      this.num = this.resultado;
+      this.num2 = '';
+    }
+  }
 }
